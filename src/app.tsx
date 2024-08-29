674cb4491460f169e75238b0ef315de891a55846
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-08-29 03:45:03 +0100

    Add button that pulls discography from Spotify API
    It's only pulling discography in the form of
    album/EP objects, without individual track info

--- src/app.tsx
+++ src/app.tsx
@@ -5,14 +5,12 @@ async function main() {
     return;
   }
 
+
   // Ensure all necessary APIs are ready
   while (!Spicetify?.showNotification || !Spicetify.ContextMenu || !Spicetify.URI) {
     await new Promise(resolve => setTimeout(resolve, 100));
   }
 
-  Spicetify.showNotification("Hello!");
-
-  const { Type } = Spicetify.URI;
 
   // Checks if it's the uri of an artist, returns true if so
   function shouldDisplayContextMenu(uris) {
@@ -23,15 +21,38 @@ async function main() {
     return false;
   }
 
-  //
-  async function getArtistDiscography(uris) {
-    const artistId = uris[0].split(":")[2];  // Extract the artist ID from the URI
 
-    // Make a request to get the artist's albums
-    const albumsResponse = await Spicetify.CosmosAsync.get(`https://api.spotify.com/v1/artists/${artistId}/albums?include_groups=album,single,compilation`);
-    const albums = albumsResponse.items;
-    return albums;
+
+  // TODO: replace with user's own market
+  // maybe replace cycle condition with the 'next' property in response
+  async function getArtistDiscography(artistUri) {
+    const artistId = artistUri[0].split(":")[2];  // Extract the artist ID from the URI
+    const limit = 50;  // Maximum limit of items per request
+    let offset = 0;
+    let totalAlbums = 0;
+    const artistDiscography = [];
+
+    do {
+      const albumsResponse = await Spicetify.CosmosAsync.get(`https://api.spotify.com/v1/artists/${artistId}/albums?market=PT&limit=${limit}&offset=${offset}&include_groups=album,single`);
+      totalAlbums = albumsResponse.total;  // The total number of albums for the artist
+      const albums = albumsResponse.items;
+
+      for (const album of albums) {
+        // Add the album data to the discography list
+        artistDiscography.push(album);
+      }
+
+      // Move to the next set of albums
+      offset += limit;
+
+    } while (totalAlbums > offset);
+
+    console.log(artistDiscography);
+
+    return artistDiscography;
   }
+
+
 
   // TODO: Add icon
   // Adds a button to the context menu of artist links
@@ -41,12 +62,7 @@ async function main() {
   ).register();
 
 
-
-
-
-
-
-
 }
+
 
 export default main;
